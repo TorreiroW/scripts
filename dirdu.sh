16387a6a7304a9c699dceda4233ffa8380a2b6a8
TorreiroW <githubwto@toorren.net>
2026-05-22 10:55:07 +0200

    update dirdu.sh: fix checkDir bug, filter error output
    - Use ${checkDir} instead of hardcoded . in find
    - Add -mindepth 1 to exclude the directory itself
    - Suppress du stderr (cannot access, permission denied)
    - Use portable shebang and quote all paths

--- dirdu.sh
+++ dirdu.sh
@@ -1,9 +1,8 @@
-#!/bin/bash
-checkDir=$1
-if [[ -z ${checkDir} ]]; then  checkDir=.; fi
-dirs=($(find . -maxdepth 1 -type d -not -name "."))
-for dir in ${dirs[@]}; do
-    if ! mountpoint -q ${dir}; then
-        du -xsm ${dir}
+#!/usr/bin/env bash
+checkDir=${1:-.}
+dirs=($(find "${checkDir}" -maxdepth 1 -mindepth 1 -type d))
+for dir in "${dirs[@]}"; do
+    if ! mountpoint -q "${dir}"; then
+        du -xsm "${dir}" 2>/dev/null
     fi
 done | sort -n
